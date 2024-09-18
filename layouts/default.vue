78e3f25f3e438e7fbd01079114f5ad0562d9e8a0
MrNedNick <mrnediala@gmail.com>
2024-09-18 20:08:50 +0200

    в

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,11 @@
         </div>
         <Menu />
       </div>
-      <ColorModeSelector />
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
+    
     <main class="p-2 mt-10">
       <slot />
     </main>
