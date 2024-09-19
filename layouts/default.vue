f7e6db93ff45ef7164ef1af1a74f974e4a86dd23
MrNedNick <mrnediala@gmail.com>
2024-09-19 13:32:47 +0200

    Add content folder

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
     <header class="flex justify-between items-center mt-5">
       <div class="flex items-center space-x-12">
         <div>
-          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
-            >Nikita Nedyalkov
+          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">
+            Nikita Nedyalkov
           </NuxtLink>
         </div>
         <Menu />
@@ -13,7 +13,6 @@
         <ColorModeSelector />
       </ClientOnly>
     </header>
-    
     <main class="p-2 mt-10">
       <slot />
     </main>
--- pages/blog/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>We are inside blog.</div>
-  <div>Post id is {{ route.params.id }}</div>
-</template>
-
-<script setup>
-const route = useRoute();
-console.log("route", route);
-</script>
