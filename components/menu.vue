b6c23b1679765c05f2c5df06322a4a021c778840
MrNedNick <mrnediala@gmail.com>
2024-10-26 18:08:19 +0200

    fix

--- components/menu.vue
+++ components/menu.vue
@@ -8,6 +8,7 @@
         <NuxtLink to="/about" class="link">About</NuxtLink>
       </li>
       <li>
+        
         <NuxtLink to="/projects" class="link">Projects</NuxtLink>
       </li>
     </ul>
