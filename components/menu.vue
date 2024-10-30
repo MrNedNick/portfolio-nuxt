91cb41b1e240b0aa18ad9f95421f43cf6ff876aa
MrNedNick <mrnediala@gmail.com>
2024-10-30 14:53:03 +0100

    k

--- components/menu.vue
+++ components/menu.vue
@@ -3,7 +3,7 @@
     <ul class="flex space-x-4">
       <li>
         <NuxtLink to="/" class="link">Main</NuxtLink>
-        
+        \
       </li>
       <li>
         <NuxtLink to="/about" class="link">About</NuxtLink>
