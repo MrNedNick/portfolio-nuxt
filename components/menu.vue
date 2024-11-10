3a7520cf95d6e09566405deeb55edd82ca4339a6
MrNedNick <mrnediala@gmail.com>
2024-11-10 18:07:23 +0100

    о

--- components/menu.vue
+++ components/menu.vue
@@ -11,6 +11,7 @@
         <NuxtLink to="/about" class="link">About</NuxtLink> 
       </li>
       <li>
+        
         <NuxtLink to="/projects" class="link">Projects</NuxtLink>
       </li>
     </ul>
