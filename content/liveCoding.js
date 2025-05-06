e03949341e476118626fea2752a1895c26465def
MrNedNick <mrnediala@gmail.com>
2025-05-06 12:20:29 +0200

    Create closure

--- content/liveCoding.js
+++ content/liveCoding.js
@@ -1,22 +1,14 @@
-function areAnagrams(str1, str2) {
-  const arrStr1 = str1.toLowerCase()
-  const arrStr2 = str2.toLowerCase()
-  let counter1 = {}
-  for (let i = 0; i < arrStr1.length; i++) {
-    const char = arrStr1[i];
-    counter1[char] = (counter1[char] || 0) + 1;
-  }
-  let counter2 = {}
-  for (let i = 0; i < arrStr2.length; i++) {
-    const char = arrStr2[i];
-    counter2[char] = (counter2[char] || 0) + 1;
-  }
-  if (Object.keys(counter1).length !== Object.keys(counter2).length) return false
-  for (let key in counter1)  {
-    if (counter1[key] !== counter2[key]) return false
-  }
-  return true
+// timeout-closure.js
+
+console.log("\n=== TIMEOUT CLOSURE ===");
+
+function delayedPrinter(msg, delay) {
+  console.log(`Setting up timer for "${msg}" (${delay}ms)`);
+
+  setTimeout(() => {
+    console.log(`>> Timer finished: ${msg}`);
+  }, delay);
 }
-чddсв
-console.log(areAnagrams('listen', 'silent')) 
-console.log(areAnagrams('hello', 'world')) 
+
+delayedPrinter("Hello after 1 second", 1000);
+delayedPrinter("Hello after 2 seconds", 2000);
